9be8e8beb8100028cd455b0ca56557a0ad44184e
samuelladyanov <samuel.ladyanov@gmail.com>
2022-12-20 14:33:59 -0800

    updated

--- src/run_audio.py
+++ src/run_audio.py
@@ -44,7 +44,7 @@ def process_video(video_id, folder, save_output):
 
         if folder == '':
             #send json to web
-            #API_ENDPOINT = "https://glimpse-ouqbsh7ia-survai.vercel.app/api/audio-detections"
+            #API_ENDPOINT = ""
             #r = requests.post(url=API_ENDPOINT, json=transcript)
             print("data sent to AWS")
 
